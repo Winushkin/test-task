8d0e73750dd8ee7970580b4546d1effed1a4df9c
Kirill Belyaev <kirill10121415@gmail.com>
2026-02-13 20:08:36 +0300

    feat(parser): add parser package

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,24 @@ package main
 
 import(
 	"file-manager/internal/config"
-	"fmt"
+	"file-manager/internal/parser"
+	"file-manager/internal/report"
+	"log"
+	
+	"context"
 )
 
 
 func main(){
+
 	cfg, err := config.NewConfig()
 	if err != nil{
-		fmt.Println(err)
+		log.Fatal(cfg, err)
 		return
 	}
-	fmt.Println(cfg)
+	
+	parsedFile, err := parser.ParseTSVFile("test_file.tsv")
+	if err != nil{
+		log.Fatal(err)
+	}
 }
