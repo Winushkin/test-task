50309984f3fd67c62ef377d6a965f6f865bb2096
Kirill Belyaev <kirill10121415@gmail.com>
2026-02-15 18:14:02 +0300

    feat(config): add filepaths in app config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,31 @@
-//Package config package used for upload app configuration
+// Package config package used for upload app configuration
 package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
+
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
-type Config struct{
-	PostgresUser string `env:"POSTGRES_USER"`
-	PostgresName string `env:"POSTRGES_NAME"`
+type Config struct {
+	PostgresUser     string `env:"POSTGRES_USER"`
+	PostgresName     string `env:"POSTRGES_NAME"`
 	PostgresPassword string `env:"POSTGRES_PASSWORD"`
-	PostgresPort int `env:"POSTGRES_PORT"`
-	PostgresHost string `env:"POSTGRES_HOST"`
-
-	DirPath string `env:"DIR_PATH"`
+	PostgresPort     int    `env:"POSTGRES_PORT"`
+	PostgresHost     string `env:"POSTGRES_HOST"`
+	ReportsDirPath   string `env:"REPORT_DIR_PATH"`
+	TSVDirPath       string `env:"TSV_DIR_PATH"`
+	PollinInterval   int    `env:"POLLING_SECONDS_INTERVAL"`
 }
 
 func NewConfig() (*Config, error) {
-	if err := godotenv.Load(); err != nil{
+	if err := godotenv.Load(); err != nil {
 		return nil, fmt.Errorf("Load: %w", err)
 	}
 
 	var cfg Config
-	if err := cleanenv.ReadEnv(&cfg); err != nil{
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		return nil, fmt.Errorf("ReadEnv: %w", err)
 	}
 
