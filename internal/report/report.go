d0499cda06247b7707d5e555ae746518dd219e3f
Kirill Belyaev <kirill10121415@gmail.com>
2026-02-15 18:08:11 +0300

    feat(report): add pdf formattin in sep func

--- internal/report/report.go
+++ internal/report/report.go
@@ -1,34 +1,41 @@
-//Package report creates reports of tsv files' records in pdf files
+// Package report creates reports of tsv files' records in pdf files
 package report
 
-import(
-	"codeberg.org/go-pdf/fpdf"
+import (
 	"file-manager/internal/parser"
-	"reflect"
 	"fmt"
+	"reflect"
+
+	"codeberg.org/go-pdf/fpdf"
 )
 
 const fontFilePath = "./internal/report/fonts/TikTokSans_18pt_Condensed-Bold.ttf"
 const eightSpaces = "        "
-func CreateReportsFromFile(records []parser.Record, dirPath string) error{
-	for _, record := range records{
+
+func CreateReportsFromFile(records []parser.Record, dirPath string) error {
+	for _, record := range records {
 		err := createReport(record, dirPath)
-		if err != nil{
+		if err != nil {
 			return fmt.Errorf("createReport: %w", err)
 		}
 	}
 	return nil
 }
 
-func createReport(record parser.Record, dirPath string) (error){
+func createPdfTemplate() fpdf.Pdf {
 	pdf := fpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.AddUTF8Font("Sans", "", fontFilePath)
 	pdf.SetFont("Sans", "", 10)
+	return pdf
+}
+
+func createReport(record parser.Record, dirPath string) error {
+	pdf := createPdfTemplate()
 
 	t := reflect.TypeOf(record)
 	v := reflect.ValueOf(record)
-	for i := 0; i < t.NumField(); i++{
+	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 		name := field.Name
 		info := field.Tag.Get("info")
@@ -37,9 +44,9 @@ func createReport(record parser.Record, dirPath string) (error){
 		pdf.Ln(5)
 
 		var displayValue string
-		if !v.Field(i).IsZero(){
+		if !v.Field(i).IsZero() {
 			displayValue = fmt.Sprintf("%v", v.Field(i).Interface())
-		}else{
+		} else {
 			displayValue = "-"
 		}
 		pdf.Cell(40, 20, fmt.Sprintf("%s%s", eightSpaces, displayValue))
